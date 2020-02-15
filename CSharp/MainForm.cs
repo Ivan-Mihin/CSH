0fb4802606adadfe33c487c3d844c31050a56ce8
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2020-02-15 13:33:38 +0100

    Omogućeno kretanje slike po prozoru

--- CSharp/MainForm.cs
+++ CSharp/MainForm.cs
@@ -13,9 +13,113 @@ namespace CSharp
 {
     public partial class MainForm : Form
     {
+        public int Movement { get; set; } = 5;
+
+        bool key_UpArrow,    key_W = false;
+        bool key_LeftArrow,  key_A = false;
+        bool key_DownArrow,  key_S = false;
+        bool key_RightArrow, key_D = false;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            {
+                key_UpArrow = true;
+                key_W = true;
+                ImageMovement();
+            }
+
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                key_LeftArrow = true;
+                key_A = true;
+                ImageMovement();
+            }
+
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                key_DownArrow = true;
+                key_S = true;
+                ImageMovement();
+            }
+
+            if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                key_RightArrow = true;
+                key_D = true;
+                ImageMovement();
+            }
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            {
+                key_UpArrow = false;
+                key_W = false;
+            }
+
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                key_LeftArrow = false;
+                key_A = false;
+            }
+
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                key_DownArrow = false;
+                key_S = false;
+            }
+
+            if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                key_RightArrow = false;
+                key_D = false;
+            }
+        }
+
+        void ImageMovement()
+        {
+            if (key_UpArrow || key_W)
+            {
+                if (bitmapSlika.Location.Y - Movement > 0)
+                {
+                    bitmapSlika.Location = new Point(bitmapSlika.Location.X, bitmapSlika.Location.Y - Movement);
+                }
+            }
+
+            if (key_LeftArrow || key_A)
+            {
+                if (bitmapSlika.Location.X - Movement > 0)
+                {
+                    bitmapSlika.Location = new Point(bitmapSlika.Location.X - Movement, bitmapSlika.Location.Y);
+                }
+            }
+
+            if (key_DownArrow || key_S)
+            {
+                if (bitmapSlika.Location.Y + Movement < background.Height - bitmapSlika.Height)
+                {
+                    bitmapSlika.Location = new Point(bitmapSlika.Location.X, bitmapSlika.Location.Y + Movement);
+                }
+            }
+
+            if (key_RightArrow || key_D)
+            {
+                if (bitmapSlika.Location.X + Movement < background.Width - bitmapSlika.Width)
+                {
+                    bitmapSlika.Location = new Point(bitmapSlika.Location.X + Movement, bitmapSlika.Location.Y);
+                }
+            }
         }
 
         private void ImageResizeToolStripMenuItem_Click(object sender, EventArgs e)
