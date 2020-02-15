73390b16ec99149d5f0e9908caa6c2ec4a5d4763
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2020-02-15 19:18:36 +0100

    Dodana funkcija ResizeImage

--- CSharp/ImageResizeForm.cs
+++ CSharp/ImageResizeForm.cs
@@ -12,13 +12,28 @@ namespace CSharp
 {
     public partial class ImageResizeForm : Form
     {
+        public Image NewBitmap { get; set; }
+
         public ImageResizeForm()
         {
             InitializeComponent();
         }
 
+        private static Image ResizeImage(Image bitmap, int width, int height)
+        {
+            Image resizedBitmap = new Bitmap(width, height);
+
+            using (Graphics GFX = Graphics.FromImage((Bitmap)resizedBitmap))
+            {
+                GFX.DrawImage(bitmap, new Rectangle(0, 0, width, height));
+            }
+
+            return resizedBitmap;
+        }
+
         private void OK_Button_ImageResizeForm_Click(object sender, EventArgs e)
         {
+            NewBitmap = ResizeImage(NewBitmap, Convert.ToInt32(widthTextBox.Text), Convert.ToInt32(heightTextBox.Text));
             this.Close();
         }
 
